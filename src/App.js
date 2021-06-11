dd31b8dc3cd90cd0efe965fef93bdd9535f6712b
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-06-11 16:52:03 +0300

    Updated v3.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome To Humphreys' Online Shop 🔥🔥</p>
+        <p>Welcome To Humphrey's Online Shop 🔥🔥</p>
       </header>
     </div>
   );
