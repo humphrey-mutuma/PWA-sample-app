3bedfd9c02f57a4e1e7b5dc9337acd5e44cc44fe
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-06-11 11:01:45 +0300

    pwa test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is me bros!! pwa @teamhumphrey 
         </a>
       </header>
     </div>
