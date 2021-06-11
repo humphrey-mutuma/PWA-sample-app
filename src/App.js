dc92b2606055ce939282a2a4a35454e6a4e3beda
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-06-11 16:31:08 +0300

    Updated version

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is me bros!! pwa @teamhumphrey 
-        </a>
+        <p>Welcome To Humphreys' Online Shop 🔥🔥</p>
       </header>
     </div>
   );
